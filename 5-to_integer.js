b815ee1d1e47355115869ec9a253c7389dd6dc6e
Peter Gatonye <petrgatonye@gmail.com>
2025-05-28 10:16:16 +0300

    Not a number

---
+++ 5-to_integer.js
@@ -0,0 +1,7 @@
+if (process.argv[2] === undefined) {
+  console.log('Not a number');
+} else if (isNaN(process.argv[2])) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + parseInt(process.argv[2]));
+}
